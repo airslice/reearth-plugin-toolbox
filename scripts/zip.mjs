3400f41473a020c13846df342722bd68f58b4d1e
lby <icesunex@hotmail.com>
2022-08-08 13:37:06 +0800

    feat: add licence and readme

--- scripts/zip.mjs
+++ scripts/zip.mjs
@@ -6,8 +6,11 @@ import Yml from "yml";
 
 const root = path.resolve("./");
 
-const pluginInfo = Yml.load(`${root}/dist/plugin/reearth.yml`);
+const pluginInfo = Yml.load(`${root}/public/reearth.yml`);
 const filename = `${pluginInfo.id}-${pluginInfo.version}.zip`;
+
+fs.copyFileSync(`${root}/README.md`, `${root}/dist/plugin/README.md`);
+fs.copyFileSync(`${root}/LICENSE`, `${root}/dist/plugin/LICENSE`);
 
 const output = fs.createWriteStream(`${root}/package/${filename}`);
 const archive = archiver("zip", { zlib: { level: 9 } });
