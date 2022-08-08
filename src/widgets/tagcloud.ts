96aafd4ce3ba57453ad3036fff304bd840754080
lby <icesunex@hotmail.com>
2022-08-08 16:14:26 +0800

    feat: init tagcloud

---
+++ src/widgets/tagcloud.ts
@@ -0,0 +1,221 @@
+import html from "../../dist/web/tagcloud/index.html?raw";
+import type { Layer } from "../apiType";
+import type { pluginMessage, actHandles } from "../type";
+
+(globalThis as any).reearth.ui.show(html, { width: 312, height: 46 });
+
+type Tag = {
+  id: string;
+  name: string;
+  layerIds: string[];
+};
+
+type TagGroup = {
+  id: string;
+  name: string;
+  tags: Tag[];
+};
+
+const addTagData = (
+  tags: TagGroup[],
+  groupId: string,
+  groupName: string,
+  tagId: string,
+  tagName: string,
+  layerId: string,
+  allowedTagGroupName?: string
+) => {
+  if (allowedTagGroupName && groupName !== allowedTagGroupName) return;
+  let tg = tags.find((g) => g.id === groupId);
+  if (!tg) {
+    tags.push({
+      id: groupId,
+      name: groupName,
+      tags: [],
+    });
+  }
+  tg = tags.find((g) => g.id === groupId);
+  let tag = tg?.tags.find((t) => t.id === tagId);
+  if (!tag) {
+    tg?.tags.push({
+      id: tagId,
+      name: tagName,
+      layerIds: [],
+    });
+  }
+  tag = tg?.tags.find((t) => t.id === tagId);
+  tag?.layerIds.push(layerId);
+};
+
+const processLayer = (
+  layer: Layer,
+  tags: TagGroup[],
+  tarTagGroupName: string,
+  otherLayerIds: string[]
+) => {
+  if (layer.children) {
+    layer.children.forEach((cl) => {
+      processLayer(cl, tags, tarTagGroupName, otherLayerIds);
+    });
+  } else {
+    let belongsToOthers = true;
+    if (layer.tags) {
+      layer.tags.forEach((tag) => {
+        if (tag.tags) {
+          if (tag.label === tarTagGroupName) {
+            belongsToOthers = false;
+          }
+          tag.tags.forEach((ct) => {
+            addTagData(
+              tags,
+              tag.id,
+              tag.label,
+              ct.id,
+              ct.label,
+              layer.id,
+              tarTagGroupName
+            );
+          });
+        } else {
+          if ("Default" === tarTagGroupName) {
+            belongsToOthers = false;
+          }
+          addTagData(
+            tags,
+            "default",
+            "Default",
+            tag.id,
+            tag.label,
+            layer.id,
+            tarTagGroupName
+          );
+        }
+      });
+    }
+    // add to others
+    if (belongsToOthers) {
+      otherLayerIds.push(layer.id);
+    }
+  }
+};
+
+const getTagsFromLayers = (layers: Layer[], tarTagGroupName: string) => {
+  if (!layers) return [];
+  const tags: TagGroup[] = [];
+  const otherLayerIds: string[] = [];
+
+  layers.forEach((layer) => {
+    processLayer(layer, tags, tarTagGroupName, otherLayerIds);
+  });
+
+  tags.forEach((tagGroup) => {
+    tagGroup.tags.sort((a, b) => (a.name > b.name ? 1 : -1));
+  });
+
+  tags.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+  if (otherLayerIds) {
+    tags
+      .find((tg) => tg.name === tarTagGroupName)
+      ?.tags.push({
+        id: "others",
+        name: "...",
+        layerIds: otherLayerIds,
+      });
+  }
+
+  const defaultGroup = tags.find((tg) => tg.id === "default");
+  if (!defaultGroup) return tags;
+
+  const otherGroups = tags.filter((tg) => tg.id !== "default");
+  return [defaultGroup, ...otherGroups];
+};
+
+const processLayerForId = (layer: Layer, allLayerIds: string[]) => {
+  if (layer.children) {
+    layer.children.forEach((cl) => {
+      processLayerForId(cl, allLayerIds);
+    });
+  } else {
+    allLayerIds.push(layer.id);
+  }
+};
+
+const getAllLayerIds = () => {
+  const allLayerIds: string[] = [];
+  const layers = (globalThis as any).reearth.layers.layers;
+  if (!layers) return allLayerIds;
+  layers.forEach((layer: Layer) => {
+    processLayerForId(layer, allLayerIds);
+  });
+
+  return allLayerIds;
+};
+
+const forceRerender = () => {
+  (globalThis as any).reearth.visualizer.camera.flyTo(
+    (globalThis as any).reearth.visualizer.camera.position
+  );
+};
+
+const handles: actHandles = {
+  getTags: () => {
+    const tarTagGroupName =
+      (globalThis as any).reearth.widget.property &&
+      (globalThis as any).reearth.widget.property.default
+        ? (globalThis as any).reearth.widget.property.default.tgname || ""
+        : "";
+
+    const layers = (globalThis as any).reearth.layers.layers;
+    const tagsdata = tarTagGroupName
+      ? getTagsFromLayers(layers, tarTagGroupName)
+      : [];
+
+    (globalThis as any).reearth.ui.postMessage({
+      act: "tags",
+      payload: tagsdata,
+    });
+  },
+  showLayers: (layerIds: string[]) => {
+    (globalThis as any).reearth.layers.show(...layerIds);
+    forceRerender();
+  },
+  hideLayers: (layerIds: string[]) => {
+    (globalThis as any).reearth.layers.hide(...layerIds);
+    forceRerender();
+  },
+  showTag: ({ tagId, show }: { tagId: string; show: boolean }) => {
+    const layers = (globalThis as any).reearth.layers.findByTags(tagId);
+    if (!layers) return;
+    const layerIds = layers.map((l: Layer) => l.id);
+    if (show) {
+      (globalThis as any).reearth.layers.show(...layerIds);
+    } else {
+      (globalThis as any).reearth.layers.hide(...layerIds);
+    }
+    //
+    forceRerender();
+  },
+  showAll: () => {
+    const allLayerIds = getAllLayerIds();
+    (globalThis as any).reearth.layers.show(...allLayerIds);
+    forceRerender();
+  },
+  hideAll: () => {
+    const allLayerIds = getAllLayerIds();
+    (globalThis as any).reearth.layers.hide(...allLayerIds);
+  },
+  resize: (size: any) => {
+    (globalThis as any).reearth.ui.resize(...size);
+  },
+};
+
+(globalThis as any).reearth.on("message", (msg: pluginMessage) => {
+  if (msg?.act) {
+    handles[msg.act]?.(msg.payload);
+  }
+});
+
+(globalThis as any).reearth.on("update", () => {
+  handles.getTags?.();
+});
---
+++ web/components/pages/tagcloud/App.tsx
@@ -0,0 +1,208 @@
+import Button from "@web/components/atoms/Button";
+import EmptyInfo from "@web/components/atoms/EmptyInfo";
+import Group from "@web/components/atoms/Group";
+import Line from "@web/components/atoms/Line";
+import Panel from "@web/components/molecules/Panel";
+import type { actHandles } from "@web/types";
+import { useEffect, useState, useCallback, useMemo, useReducer } from "react";
+
+import { mockTagsData } from "./mock";
+
+import "@web/components/molecules/Common/common.css";
+import "./app.css";
+
+type Tag = {
+  id: string;
+  name: string;
+  layerIds: string[];
+};
+
+export type TagGroup = {
+  id: string;
+  name: string;
+  tags: Tag[];
+};
+
+const App = () => {
+  const [tags, setTags] = useState<TagGroup[]>([]);
+  const [tagStatus] = useState(new Map());
+  const [layerTags] = useState(new Map());
+  const [tagLayers] = useState(new Map());
+
+  const updateReducer = useCallback(
+    (num: number): number => (num + 1) % 1_000_000,
+    []
+  );
+  const [, forceUpdate] = useReducer(updateReducer, 0);
+
+  const toggleTag = useCallback(
+    (tagId: string) => {
+      const status = tagStatus.get(tagId);
+      const show = !status;
+      if (show) {
+        (globalThis as any).parent.postMessage(
+          {
+            act: "showLayers",
+            payload: tagLayers.get(tagId),
+          },
+          "*"
+        );
+      } else {
+        const hideLayerIds: string[] = [];
+        const layerIds = tagLayers.get(tagId);
+        layerIds.forEach((lid: string) => {
+          const tagIds = layerTags
+            .get(lid)
+            .filter((tid: string) => tid !== tagId);
+          let canHide = true;
+          tagIds.forEach((tid: string) => {
+            if (tagStatus.get(tid) === true) canHide = false;
+          });
+          if (canHide) hideLayerIds.push(lid);
+        });
+        if (hideLayerIds) {
+          (globalThis as any).parent.postMessage(
+            {
+              act: "hideLayers",
+              payload: hideLayerIds,
+            },
+            "*"
+          );
+        }
+      }
+
+      tagStatus.set(tagId, !status);
+      forceUpdate();
+    },
+    [tagStatus, layerTags, tagLayers]
+  );
+
+  const showAll = useCallback(() => {
+    tagStatus.forEach((v, k) => {
+      tagStatus.set(k, true);
+    });
+    (globalThis as any).parent.postMessage(
+      {
+        act: "showAll",
+      },
+      "*"
+    );
+    forceUpdate();
+  }, [tagStatus]);
+
+  const hideAll = useCallback(() => {
+    tagStatus.forEach((v, k) => {
+      tagStatus.set(k, false);
+    });
+    (globalThis as any).parent.postMessage(
+      {
+        act: "hideAll",
+      },
+      "*"
+    );
+    forceUpdate();
+  }, [tagStatus]);
+
+  const onResize = (width: number, height: number) => {
+    (globalThis as any).parent.postMessage(
+      {
+        act: "resize",
+        payload: [width, height],
+      },
+      "*"
+    );
+  };
+
+  const processTagsData = useCallback(
+    (tagsdata: TagGroup[]) => {
+      layerTags.clear();
+      tagsdata.forEach((tg) => {
+        tg.tags.forEach((t) => {
+          if (!tagStatus.has(t.id)) {
+            tagStatus.set(t.id, true);
+          }
+
+          tagLayers.set(t.id, t.layerIds);
+
+          t.layerIds.forEach((lid) => {
+            if (!layerTags.has(lid)) {
+              layerTags.set(lid, []);
+            }
+            layerTags.get(lid).push(t.id);
+          });
+        });
+      });
+
+      setTags(tagsdata);
+    },
+    [layerTags, tagLayers, tagStatus]
+  );
+
+  const actHandles: actHandles = useMemo(() => {
+    return {
+      tags: processTagsData,
+    };
+  }, [processTagsData]);
+
+  const init = useCallback(() => {
+    (globalThis as any).addEventListener("message", (msg: any) => {
+      if (msg.source !== (globalThis as any).parent || !msg.data.act) return;
+      actHandles[msg.data.act as keyof actHandles]?.(msg.data.payload);
+    });
+
+    (globalThis as any).parent.postMessage({ act: "getTags" }, "*");
+
+    if (import.meta.env.MODE === "development") {
+      processTagsData(mockTagsData);
+    }
+  }, [actHandles, processTagsData]);
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
+  return (
+    <Panel title="Tag Cloud" icon="tag" onResize={onResize}>
+      <>
+        {tags.length > 0 ? (
+          <>
+            {tags.map((tagGroup) => (
+              <Group key={tagGroup.id} noBorder={true}>
+                {tagGroup.tags.map((tag) => (
+                  <Button
+                    text={tag.name}
+                    key={tag.id}
+                    compact={true}
+                    buttonType="tag"
+                    status={tagStatus.get(tag.id) ? "on" : "off"}
+                    onClick={() => {
+                      toggleTag(tag.id);
+                    }}
+                  />
+                ))}
+              </Group>
+            ))}
+            <Line>
+              <Button
+                text="Show All"
+                buttonType="tag"
+                buttonStyle="secondary"
+                onClick={showAll}
+              />
+              <Button
+                text="Hide All"
+                buttonType="tag"
+                buttonStyle="secondary"
+                onClick={hideAll}
+              />
+            </Line>
+          </>
+        ) : (
+          <EmptyInfo text="NO TAG GROUP SELECTED" />
+        )}
+      </>
+    </Panel>
+  );
+};
+
+export default App;
