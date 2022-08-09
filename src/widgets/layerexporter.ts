fb74bff077216b9dbec5516eccdaaae527069733
lby <icesunex@hotmail.com>
2022-08-09 16:38:58 +0800

    feat: dev layer exporter

--- src/widgets/layerexporter.ts
+++ src/widgets/layerexporter.ts
@@ -1,14 +1,100 @@
 import html from "../../dist/web/layerexporter/index.html?raw";
+import type { Layer } from "../apiType";
 import type { pluginMessage, actHandles } from "../type";
 
 (globalThis as any).reearth.ui.show(html, { width: 312, height: 46 });
+
+type LayerData = { [index: string]: any };
+
+const processProperty = (
+  obj: any,
+  key: string,
+  property: LayerData,
+  name: string,
+  propertyNames: string[],
+  extensionId = ""
+) => {
+  const currentName = name === "" ? `${extensionId}.${key}` : `${name}.${key}`;
+  if (typeof obj[key] === "object") {
+    Object.keys(obj[key]).forEach((k) => {
+      processProperty(
+        obj[key],
+        k,
+        property,
+        currentName,
+        propertyNames,
+        extensionId
+      );
+    });
+  } else {
+    if (
+      !currentName.includes("location.lat") &&
+      !currentName.includes("location.lng") &&
+      !(currentName in property)
+    ) {
+      property[currentName] = obj[key];
+      if (!propertyNames.includes(currentName)) {
+        propertyNames.push(currentName);
+      }
+    }
+  }
+};
+
+const processLayer = (
+  layer: Layer,
+  layersdata: LayerData[],
+  propertyNames: string[]
+) => {
+  if (layer.children) {
+    layer.children.forEach((l) => processLayer(l, layersdata, propertyNames));
+  } else {
+    const property = {
+      title: layer.title,
+      lng: layer.property?.default?.location?.lng,
+      lat: layer.property?.default?.location?.lat,
+    };
+    // property.set("title", layer.title);
+    // property.set("longitude", layer.property?.default?.location?.lng);
+    // property.set("latitude", layer.property?.default?.location?.lat);
+
+    Object.keys(layer.property?.default).forEach((key) => {
+      processProperty(
+        layer.property.default,
+        key,
+        property,
+        "",
+        propertyNames,
+        layer.extensionId
+      );
+    });
+
+    layersdata.push(property);
+  }
+};
 
 const handles: actHandles = {
   resize: (size: any) => {
     (globalThis as any).reearth.ui.resize(...size);
   },
-  flyTo: (target: any) => {
-    (globalThis as any).reearth.visualizer.camera.flyTo(target);
+  layersdata: ({ type }: { type: string }) => {
+    const layers = (globalThis as any).reearth.layers.layers;
+    const layersdata: LayerData[] = [];
+    const propertyNames: string[] = ["title", "lng", "lat"];
+
+    if (layers) {
+      layers.forEach((layer: Layer) => {
+        processLayer(layer, layersdata, propertyNames);
+      });
+    }
+
+    (globalThis as any).reearth.ui.postMessage({
+      act: "layersdata",
+      payload: {
+        type,
+        layersdata,
+        propertyNames,
+      },
+    });
   },
 };
 
@@ -16,11 +102,4 @@ const handles: actHandles = {
   if (msg?.act) {
     handles[msg.act]?.(msg.payload);
   }
-});
-
-(globalThis as any).reearth.on("mousemove", (mousedata: any) => {
-  (globalThis as any).reearth.ui.postMessage({
-    act: "mousemove",
-    payload: mousedata,
-  });
 });
--- web/components/pages/layerexporter/App.tsx
+++ web/components/pages/layerexporter/App.tsx
@@ -2,24 +2,75 @@ import Button from "@web/components/atoms/Button";
 import EmptyInfo from "@web/components/atoms/EmptyInfo";
 import Panel from "@web/components/molecules/Panel";
 import type { actHandles } from "@web/types";
-import { useCallback, useEffect, useState, useMemo } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 
 import "@web/components/molecules/Common/common.css";
 import "./app.css";
 
+type LayerData = { [index: string]: any };
+
 const App = () => {
-  const [mouseLocation, setMouseLocation] = useState({ lat: 0, lng: 0 });
+  const downlaodCSV = useCallback(
+    (layersdata: LayerData[], propertyNames: string[]) => {
+      let csvData = "";
+
+      let row = "";
+      for (const propertyName of propertyNames) {
+        row += `${propertyName},`;
+      }
+      row += "\r\n";
+      csvData += row;
+
+      try {
+        layersdata.forEach((layerdata) => {
+          let row = "";
+          for (const propertyName of propertyNames) {
+            row += `"${
+              layerdata[propertyName] !== undefined
+                ? layerdata[propertyName]
+                : ""
+            }",`;
+          }
+          row += "\r\n";
+          csvData += row;
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
+      if (csvData) {
+        const filename = `reearth-layers-${new Date().getTime()}.csv`;
+        //
+        const alink = document.createElement("a");
+        const _utf = "\uFEFF";
+        if (window.Blob) {
+          const csvDataBlob = new Blob([_utf + csvData], {
+            type: "text/csv",
+          });
+          alink.href = URL.createObjectURL(csvDataBlob);
+        }
+        document.body.appendChild(alink);
+        alink.setAttribute("download", filename);
+        alink.click();
+        document.body.removeChild(alink);
+      }
+    },
+    []
+  );
 
   const exportCSV = () => {
     (globalThis as any).parent.postMessage(
       {
-        act: "export",
+        act: "layersdata",
+        payload: {
+          type: "csv",
+        },
       },
       "*"
     );
   };
 
-  const onResize = (width: number, height: number) => {
+  const onResize = useCallback((width: number, height: number) => {
     (globalThis as any).parent.postMessage(
       {
         act: "resize",
@@ -27,18 +78,25 @@ const App = () => {
       },
       "*"
     );
-  };
+  }, []);
 
   const actHandles: actHandles = useMemo(() => {
     return {
-      mousemove: (mousedata: any) => {
-        setMouseLocation({
-          lat: mousedata.lat ?? "n/a",
-          lng: mousedata.lng ?? "n/a",
-        });
+      layersdata: ({
+        type,
+        layersdata,
+        propertyNames,
+      }: {
+        type: string;
+        layersdata: LayerData[];
+        propertyNames: string[];
+      }) => {
+        if (type === "csv") {
+          downlaodCSV(layersdata, propertyNames);
+        }
       },
     };
-  }, []);
+  }, [downlaodCSV]);
 
   const init = useCallback(() => {
     (globalThis as any).addEventListener("message", (msg: any) => {
@@ -53,12 +111,11 @@ const App = () => {
 
   return (
     <Panel title="Layer Exporter" onResize={onResize} icon="export">
-      <EmptyInfo text="Please make settings in right panel.">
+      <EmptyInfo>
         <div>Please make settings in right panel.</div>
         <div>We now only support file export in csv format.</div>
       </EmptyInfo>
-
-      <Button text="Export" onClick={exportCSV} />
+      <Button text="Export" buttonStyle="secondary" onClick={exportCSV} />
     </Panel>
   );
 };
