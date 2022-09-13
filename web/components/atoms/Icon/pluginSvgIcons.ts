ac2e43cfa288b17139aba7c2d717fc420543dd45
lby <icesunex@hotmail.com>
2022-09-13 13:47:20 +0800

    feat: add icons

--- web/components/atoms/Icon/pluginSvgIcons.ts
+++ web/components/atoms/Icon/pluginSvgIcons.ts
@@ -1,7 +1,21 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 import { ReactComponent as PersonSimpleWalk } from "./Icons/personSimpleWalk.svg";
+import { ReactComponent as ArrowDown } from "./PluginIcons/ArrowDown.svg";
+import { ReactComponent as ArrowLeft } from "./PluginIcons/ArrowLeft.svg";
+import { ReactComponent as ArrowLineDown } from "./PluginIcons/ArrowLineDown.svg";
+import { ReactComponent as ArrowLineUp } from "./PluginIcons/ArrowLineUp.svg";
+import { ReactComponent as ArrowRight } from "./PluginIcons/ArrowRight.svg";
+import { ReactComponent as ArrowUp } from "./PluginIcons/ArrowUp.svg";
+import { ReactComponent as Crosshair } from "./PluginIcons/Crosshair.svg";
 
 export default {
   pedestrian: PersonSimpleWalk,
+  crosshair: Crosshair,
+  arrowDown: ArrowDown,
+  arrowLeft: ArrowLeft,
+  arrowLineDown: ArrowLineDown,
+  arrowLineUp: ArrowLineUp,
+  arrowRight: ArrowRight,
+  arrowUp: ArrowUp,
 };
