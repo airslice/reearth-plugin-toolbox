63fb7e48182d227e373dd1380a25ea0ba929fcf9
lby <icesunex@hotmail.com>
2022-09-14 17:16:24 +0800

    feat: add theme pt.1

---
+++ web/theme/themes/dark.ts
@@ -0,0 +1,10 @@
+import { commonTheme } from "../common";
+
+export default {
+  ...commonTheme,
+  colors: {
+    background: "#171618",
+    primary: "#3B3CD0",
+    secondary: "#4445EA",
+  },
+};
